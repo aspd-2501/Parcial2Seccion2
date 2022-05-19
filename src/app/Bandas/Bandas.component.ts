cfb581c6779fd5496746c50abfe4959d9202c6ac
aspd-2501 <78044651+aspd-2501@users.noreply.github.com>
2022-05-19 14:09:34 -0500

    modulo

---
+++ src/app/Bandas/Bandas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Bandas',
+  templateUrl: './Bandas.component.html',
+  styleUrls: ['./Bandas.component.css']
+})
+export class BandasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/Bandas/Bandas.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BandasComponent } from './Bandas.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BandasComponent]
+})
+export class BandasModule { }
